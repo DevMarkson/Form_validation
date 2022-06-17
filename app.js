49f7cca2628350b6fa2382615e49771c2e533196
Markson17 <marksonfrosh@gmail.com>
2022-06-17 02:31:56 +0100

    first commit

--- app.js
+++ app.js
@@ -7,38 +7,55 @@ const first_name_error = document.querySelector(".first_name_error");
 const last_name_error = document.querySelector(".last_name_error");
 const email_error = document.querySelector(".email_error");
 const pwd_error = document.querySelector(".pwd_error");
+const img = document.querySelectorAll('img')
+
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  if (
-    first_name.value === "" ||
-    last_name === "" ||
-    email.value === "" ||
-    pwd.value === ""
-  ) {
-      console.log('empty');
-    first_name_error.textContent = "First Name cannot be empty";
-    last_name_error.textContent = "Last Name cannot be empty";
-    email_error.textContent = "Looks like this is not an email";
-    pwd_error.textContent = "Password cannot be empty";
-    first_name.style.borderColor = "hsl(0, 100%, 74%)";
-    last_name.style.borderColor = "hsl(0, 100%, 74%)";
-    email.style.borderColor = "hsl(0, 100%, 74%)";
-    pwd.style.borderColor = "hsl(0, 100%, 74%)";
+  if(first_name.value === '') {
+    first_name_error.textContent = 'First Name cannot be blank'
+    last_name_error.textContent = 'Last Name cannot be blank'
+    email_error.textContent = 'Not a valid email'
+    pwd_error.textContent = 'Password cannot be blank'
+    img.forEach((item) => {
+      item.classList.add('show')
+    })
+  } else {
+    first_name_error.remove()
+    last_name_error.remove()
+    email_error.remove()
+    pwd_error.remove()
   }
-
-  else {
-    console.log("not");
-    first_name.style.borderColor = "hsl(246, 25%, 77%)";
-    last_name.style.borderColor = "hsl(246, 25%, 77%)";
-    email.style.borderColor = "hsl(246, 25%, 77%)";
-    pwd.style.borderColor = "hsl(246, 25%, 77%)";
-    first_name_error.style.display = "none";
-    last_name_error.style.display = "none";
-    email_error.style.display = "none";
-    pwd_error.style.display = "none";
-  }
-  
 });
+
+first_name.onchange = function() {
+  if(first_name.value !== '') {
+    first_name_error.remove()
+  } else {
+    console.log('zuri');
+  }
+}
+last_name.onchange = function() {
+  if(first_name.value !== '') {
+    last_name_error.remove()
+  } else {
+    console.log('zuri');
+  }
+}
+email.onchange = function() {
+  if(first_name.value !== '') {
+    email_error.remove()
+  } else {
+    console.log('zuri');
+  }
+}
+pwd.onchange = function() {
+  if(pwd.value !== '') {
+    pwd_error.remove()
+  } else {
+    console.log('zuri');
+  }
+}
+
 
 // if (
 //     first_name.value === "" ||
