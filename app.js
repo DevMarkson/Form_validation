b8cb45b374ace0d89a00833e495ca3794e7c0a34
Markson Favour <95889759+Markson17@users.noreply.github.com>
2022-07-02 03:26:20 +0100

    final commit

--- app.js
+++ app.js
@@ -1,68 +1,62 @@
-const first_name = document.getElementById("name");
-const last_name = document.getElementById("lname");
+const form = document.getElementById("form");
+const fname = document.getElementById("fname");
+const lname = document.getElementById("lname");
 const email = document.getElementById("email");
-const pwd = document.getElementById("pwd");
-const form = document.querySelector("form");
-const first_name_error = document.querySelector(".first_name_error");
-const last_name_error = document.querySelector(".last_name_error");
-const email_error = document.querySelector(".email_error");
-const pwd_error = document.querySelector(".pwd_error");
-const img = document.querySelectorAll('img')
+const password = document.getElementById("password");
 
 form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  if(first_name.value === '') {
-    first_name_error.textContent = 'First Name cannot be blank'
-    last_name_error.textContent = 'Last Name cannot be blank'
-    email_error.textContent = 'Not a valid email'
-    pwd_error.textContent = 'Password cannot be blank'
-    img.forEach((item) => {
-      item.classList.add('show')
-    })
-  } else {
-    first_name_error.remove()
-    last_name_error.remove()
-    email_error.remove()
-    pwd_error.remove()
-  }
-});
+    e.preventDefault();
 
-first_name.onchange = function() {
-  if(first_name.value !== '') {
-    first_name_error.remove()
-  } else {
-    console.log('zuri');
-  }
-}
-last_name.onchange = function() {
-  if(first_name.value !== '') {
-    last_name_error.remove()
-  } else {
-    console.log('zuri');
-  }
-}
-email.onchange = function() {
-  if(first_name.value !== '') {
-    email_error.remove()
-  } else {
-    console.log('zuri');
-  }
-}
-pwd.onchange = function() {
-  if(pwd.value !== '') {
-    pwd_error.remove()
-  } else {
-    console.log('zuri');
-  }
+    checkInputs();
+})
+
+function checkInputs() {
+    const fnameValue = fname.value.trim();
+    const lnameValue = lname.value.trim();
+    const emailValue = email.value.trim();
+    const passwordValue = password.value.trim();
+
+    if (fnameValue === ""){
+        setErrorFor(fname, "Firstname cannot be blank");
+    }
+    else{
+        setSuccessFor(fname);
+    }
+
+    if (lnameValue === ""){
+        setErrorFor(lname, "Lastname cannot be blank");
+    }
+    else {
+        setSuccessFor(lname);
+    }
+
+    if (emailValue === "") {
+        setErrorFor(email, "Email cannot be blank");
+    }
+    else {
+        setSuccessFor(email);
+    }
+
+    if (passwordValue === "") {
+        setErrorFor(password, "Password cannot be blank");
+    }
+    else {
+        setSuccessFor(password);
+    }
 }
 
+function setErrorFor(input, message) {
+    const formControl = input.parentElement;
+    const small = formControl.querySelector("small");
 
-// if (
-//     first_name.value === "" ||
-//     last_name === "" ||
-//     email.value === "" ||
-//     pwd.value === ""
-//   ) {
-//     console.log("empty");
-    
-//   } 
+    small.innerText = message;
+
+    formControl.className = "form-control error";
+}
+
+function setSuccessFor(input) {
+    const formControl = input.parentElement;
+    formControl.className = "form-control success";
+}
+
+
